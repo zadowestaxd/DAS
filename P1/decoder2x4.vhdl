b80f1fa6f144e447701a7ec9e3b8350f34d8f74d
Alejandro Valencia Blancas <alezombi@gmail.com>
2023-02-08 11:17:16 +0100

    Update decoder2x4.vhdl

--- P1/decoder2x4.vhdl
+++ P1/decoder2x4.vhdl
@@ -1,25 +1,19 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity decoder2x4 is
+entity decodificador2x4 is
     port (
-        e: in std_logic;
-        i: in std_logic_vector(1 downto 0);
-        o: out std_logic_vector(3 downto 0)
+        En: in std_logic;
+        e: in std_logic_vector(1 downto 0);
+        s: out std_logic_vector(3 downto 0)
     );
-end entity decoder2x4;
+end entity decodificador2x4;
 
-architecture decoder2x4_rtl of decoder2x4 is
+architecture decodificador2x4_rtl of decodificador2x4 is
 begin    
-    with i&e select
-    o <= 
-        "0001" when "001",
-        "0010" when "011",
-        "0100" when "101",
-        "1000" when "111",
-        "0000" when "000",
-        "0000" when "010",
-        "0000" when "100",
-        "0000" when "110",
-        "1111" when others;
-end architecture decoder2x4_rtl;
+    s <=     "0001" when (e="00" and Em='1') else
+             "0010" when (e="01" and En='1') else
+             "0100" when (e="10" and En='1') else
+             "1000" when (e="11" and En='1') else
+             "0000" when En='0' else "1111";
+end architecture decodificador2x4_rtl;
